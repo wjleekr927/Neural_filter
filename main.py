b2e5e8ed55d0fd645ed8079a9f27e21166c84d43
woojun100 <wjleekr927@kaist.ac.kr>
2021-11-17 00:51:45 +0900

    main.py correction

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-# Main file
+# Import packages
+import sys
+import torch
+import numpy as np
+
+from utils.options import args_parser
+
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from tqdm import tqdm
+from tqdm import trange
+
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    # Parse args (from ./utils/options.py)
+    args = args_parser()
+
+    # Device setting
+    args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
+    
+    # Print for current option
+    print("\nTraining for following settings:")
+    print("\tEpoch: {}, Input M: {}, Model: {}, Device: {}".format(args.epochs, args.M_input, args.model, args.device))
+    
+    # tensorboard --logdir=runs
+    # tensorboard --inspect --event_file=myevents.out --tag=loss
+    print("\nDone!\n")
--- utils/options.py
+++ utils/options.py
@@ -1,2 +1,25 @@
+# Take arguments
 import argparse
 
+def args_parser():
+    parser = argparse.ArgumentParser()
+
+    # Training arguments
+    parser.add_argument('--epochs', type=int, default=5 , help="Number of epochs")
+    parser.add_argument('--bs', type=int, default=16, help="Size of batch")
+    parser.add_argument('--lr', type=float, default=1e-3, help="Learning rate")
+
+    parser.add_argument('--M_input', type=int, default=10, help="Number of input sensors")
+    parser.add_argument('--train_set_ratio', type= float, default= .7, help="Ratio of training set")
+    # parser.add_argument('--optimizer', type=str, default='SGD')
+    # parser.add_argument('--output_dir', type=str, default='./output')
+    # parser.add_argument('--summary_grad', action='store_true')
+
+    # Model arguments
+    parser.add_argument('--model', type=str, default='deep',help = "Model structure setting")
+
+    # Others
+    # parser.add_argument('--noise', action = 'store_true', help= "Add option to use noise. Defalut is not used. Also, power needed to be selected in code.")
+    parser.add_argument('--gpu', type=int, default=0, help="GPU index setting, -1 for CPU")
+    args = parser.parse_args()
+    return args
