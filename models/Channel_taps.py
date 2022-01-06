783c42572588d15c8b0e27fead1dbb9b33ac7df5
woojun100 <wjleekr927@kaist.ac.kr>
2022-01-06 21:26:42 +0900

    Channel change

--- models/Channel_taps.py
+++ models/Channel_taps.py
@@ -4,10 +4,11 @@ import numpy as np
 def channel_gen(total_taps, decay_factor, seed):
     np.random.seed(seed)
     channel_vec = np.zeros((total_taps,1), dtype = 'complex_')
-    exp_decay = np.round(np.exp(-decay_factor), 4)
-
+    # Not use exponential here
+    # exp_decay = np.round(np.exp(-decay_factor), 4)
     for idx in range(total_taps):
-        rnd_complex = np.random.randn() + np.random.randn() * 1j
-        channel_vec[idx] = np.sqrt(exp_decay**idx / 2) * rnd_complex
+        # Std to variance
+        rnd_complex = np.random.randn(scale=1/(2*total_taps)**.5) + np.random.randn(scale=1/(2*total_taps)**.5) * 1j
+        channel_vec[idx] = rnd_complex
     
     return channel_vec
