39669f8e98d41222b6653feada96eafa97b4392e
woojun100 <wjleekr927@kaist.ac.kr>
2022-01-10 16:36:09 +0900

    LF vs NF check

--- data/generate_data.sh
+++ data/generate_data.sh
@@ -1,36 +1,8 @@
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 200 --data_gen_type 'train' --rand_seed_train 9997
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 1000 --data_gen_type 'train' --rand_seed_train 9997
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 5000 --data_gen_type 'train' --rand_seed_train 9997
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 10000 --data_gen_type 'train' --rand_seed_train 9997
 python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 20000 --data_gen_type 'train' --rand_seed_train 9997
-python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 20000 --data_gen_type 'test' --rand_seed_test 4997
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 48000 --data_gen_type 'train' --rand_seed_train 9997
 
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 400 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 1000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 2000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 4000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 8000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 12000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 16000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 20000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 24000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 30000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 36000 --data_gen_type 'train' --rand_seed_train 9997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 40000 --data_gen_type 'train' --rand_seed_train 9997
-
-
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 400 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 1000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 2000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 4000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 8000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 12000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 16000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 20000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 24000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 30000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 36000 --data_gen_type 'test' --rand_seed_test 4997
-# python symbol_make.py --total_taps 10 --filter_size 5 --mod_scheme "QPSK" --gen_seq_len 40000 --data_gen_type 'test' --rand_seed_test 4997
-
-
-#python symbol_make.py --mod_scheme "QPSK" --seq_len 80000 --data_gen_type 'train' --filter_size 70 --rand_seed_train 9995
-#python symbol_make.py --mod_scheme "QPSK" --seq_len 80000 --data_gen_type 'test' --filter_size 70 --rand_seed_test 4995
-
-#python symbol_make.py --mod_scheme "QPSK" --seq_len 80000 --data_gen_type 'train' --filter_size 100 --rand_seed_train 8999
-#python symbol_make.py --mod_scheme "QPSK" --seq_len 80000 --data_gen_type 'test' --filter_size 100 --rand_seed_test 3999
+python symbol_make.py --mod_scheme "QPSK" --gen_seq_len 48000 --data_gen_type 'test' --rand_seed_test 4997
--- execute_main.sh
+++ execute_main.sh
@@ -1,38 +1,15 @@
-# python main.py --seq_len 40000 --epochs 30
-
-# python main.py --seq_len 80000 --filter_type "Linear" --filter_size 70 --rand_seed_train 9995 --rand_seed_test 4995
-# python main.py --seq_len 80000 --filter_type "NN" --filter_size 18 --epochs 30 --rand_seed_train 9993 --rand_seed_test 4993
-
 # Start simulation
 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 200 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 400 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 1000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 2000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 4000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 8000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 12000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 16000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 24000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 30000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 36000 --test_seq_len 40000
-# python main.py --total_taps 10 --filter_size 5 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 40000 --test_seq_len 40000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 200 --test_seq_len 48000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 1000 --test_seq_len 48000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 5000 --test_seq_len 48000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 10000 --test_seq_len 48000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 48000
+python main.py --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 48000 --test_seq_len 48000
 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 200 --test_seq_len 40000 --bs 16 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 400 --test_seq_len 40000 --bs 16 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 1000 --test_seq_len 40000 --bs 16 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 2000 --test_seq_len 40000 --bs 64 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 4000 --test_seq_len 40000 --bs 64 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 8000 --test_seq_len 40000 --bs 64 
-# python main.py --epochs 80 --lr 8e-4 --total_taps 10 --filter_size 24 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 20000 --bs 64 
-# python main.py --total_taps 10 --filter_size 24 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 16000 --test_seq_len 40000 --bs 64
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 40000 --bs 256 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 24000 --test_seq_len 40000 --bs 256 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 30000 --test_seq_len 40000 --bs 256 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 36000 --test_seq_len 40000 --bs 256 
-# python main.py --total_taps 10 --filter_size 5 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 40000 --test_seq_len 40000 --bs 256
-
-
-# python main.py --epochs 80 --lr 8e-4 --total_taps 10 --filter_size 24 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 200 --test_seq_len 20000 --bs 64 
-python main.py --epochs 80 --lr 8e-4 --total_taps 10 --filter_size 24 --filter_type "Linear" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 20000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 200 --test_seq_len 48000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 1000 --test_seq_len 48000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 5000 --test_seq_len 48000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 10000 --test_seq_len 48000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 20000 --test_seq_len 48000 --bs 64 
+python main.py --epochs 80 --lr 8e-4 --filter_type "NN" --rand_seed_train 9997 --rand_seed_test 4997 --train_seq_len 48000 --test_seq_len 48000 --bs 64 
